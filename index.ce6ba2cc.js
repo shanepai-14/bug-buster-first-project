2f2139a44ba78d419d1233580ba995d0e41779e8
shanepai-14 <96838519+shanepai-14@users.noreply.github.com>
2024-01-31 11:48:18 +0000

    Deploying to gh-pages from @ shanepai-14/bug-buster-first-project@b01a1c10ffd77b08f0d45da3f7e6f67b760f324e 🚀

---
+++ index.ce6ba2cc.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-review-open]"),closeModalBtn:document.querySelector("[data-modal-review-close]"),modal:document.querySelector("[data-modal-review]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.ce6ba2cc.js.map
