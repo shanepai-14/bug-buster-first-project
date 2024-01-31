6f3d93347053c2ada03ed99e3ea20a048acb28f6
shanepai-14 <shaneenriquez50@gmail.com>
2024-01-31 16:36:14 +0800

    done products section with responsive

---
+++ src/index.js
@@ -0,0 +1,21 @@
+//swiper
+var swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  slidesPerView: 4,
+  spaceBetween: 18,
+
+  breakpoints: {
+    // when window width is >= 320px
+    768: {
+      slidesPerView: 2.5,
+    },
+    1159: {
+      slidesPerView: 4,
+    },
+    300: {
+      slidesPerView: 1,
+    },
+  },
+});
