64f53251a42aa25a21ed7e857de926cf61c6c4d3
shanepai-14 <96838519+shanepai-14@users.noreply.github.com>
2024-01-31 08:49:33 +0000

    Deploying to gh-pages from @ shanepai-14/bug-buster-first-project@2ca4cff85c34d65a78a2d3156faeac33efc35ce8 🚀

---
+++ index.86d8807c.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"},slidesPerView:4,spaceBetween:18,breakpoints:{768:{slidesPerView:2.5},1159:{slidesPerView:4},300:{slidesPerView:1}}});
+//# sourceMappingURL=index.86d8807c.js.map
