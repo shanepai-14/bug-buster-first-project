7f728c427d54e2a8097ee0fe27c0d4ceb3562259
shanepai-14 <shaneenriquez50@gmail.com>
2024-02-02 05:09:32 +0800

    modal done

--- src/modal.js
+++ src/modal.js
@@ -12,3 +12,18 @@
     refs.modal.classList.toggle('is-hidden-review');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-topseller-open]'),
+    closeModalBtn: document.querySelector('[data-modal-topseller-close]'),
+    modal: document.querySelector('[data-modal-topseller]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-topseller');
+  }
+})();
