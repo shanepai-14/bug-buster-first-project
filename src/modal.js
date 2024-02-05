7e7a036ece84121c1a296c642074964791818c0d
Zurielle0520 <marcllednar@gmail.com>
2024-02-05 23:36:34 +0800

    subscribe

--- src/modal.js
+++ src/modal.js
@@ -27,3 +27,18 @@
     refs.modal.classList.toggle('is-hidden-topseller');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-subscribe-open]'),
+    closeModalBtn: document.querySelector('[data-modal-subscribe-close]'),
+    modal: document.querySelector('[data-modal-subscribe]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden-subscribe');
+  }
+})();
