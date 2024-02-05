0e095e56d9fe384c3c8c06eb9d602d6742c0bd78
shanepai-14 <96838519+shanepai-14@users.noreply.github.com>
2024-02-05 12:23:50 +0000

    Deploying to gh-pages from @ shanepai-14/bug-buster-first-project@ecb085c9f28f15d5af71daeca47b08c12beda899 🚀

--- index.2c90f008.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"},slidesPerView:4,spaceBetween:18,breakpoints:{768:{slidesPerView:2.5},1159:{slidesPerView:4},1:{slidesPerView:1}}}),new Swiper(".reviewSwiper",{pagination:{el:".swiper-pagination"},slidesPerView:3,spaceBetween:28,breakpoints:{768:{slidesPerView:2},1159:{slidesPerView:3},1:{slidesPerView:1}}});
-//# sourceMappingURL=index.2c90f008.js.map
---
+++ index.97efc804.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination"},slidesPerView:4,spaceBetween:18,autoplay:{delay:3e3},breakpoints:{768:{slidesPerView:2.5},1159:{slidesPerView:4},1:{slidesPerView:1}}}),new Swiper(".reviewSwiper",{pagination:{el:".swiper-pagination"},autoplay:{delay:3e3},slidesPerView:3,spaceBetween:28,breakpoints:{768:{slidesPerView:2},1159:{slidesPerView:3},1:{slidesPerView:1}}});
+//# sourceMappingURL=index.97efc804.js.map
