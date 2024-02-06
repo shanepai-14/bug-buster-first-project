6a28a455dbf6abc4359fb717ed5d85437638a3af
LinckyLloyd <dojillo.lloyd@yahoo.com.ph>
2024-02-05 20:46:32 -0800

    Feb. 5

--- src/index.js
+++ src/index.js
@@ -39,3 +39,41 @@ var swiper = new Swiper('.reviewSwiper', {
     },
   },
 });
+
+// Hamburger
+
+(() => {
+  const mobileMenu = document.querySelector('.js-menu-container');
+  const openMenuBtn = document.querySelector('.js-open-menu');
+  const closeMenuBtn = document.querySelector('.js-close-menu');
+
+  const toggleMenu = () => {
+    const isMenuOpen =
+      openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
+    openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
+    mobileMenu.classList.toggle('is-open');
+
+    const scrollLockMethod = !isMenuOpen
+    ? 'disableBodyScroll'
+    : 'enableBodyScroll';
+  bodyScrollLock[scrollLockMethod](document.body); 
+  };  
+
+  openMenuBtn.addEventListener('click', toggleMenu);
+  closeMenuBtn.addEventListener('click', toggleMenu);
+
+      window.matchMedia('(max-width: 767px)').addEventListener('change', e => {
+    if (!e.matches) return;
+
+      openMenuBtn.style.display = 'block';
+   
+  });
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
+    mobileMenu.classList.remove('is-open');
+      openMenuBtn.setAttribute('aria-expanded', false);
+      openMenuBtn.style.display = 'none';
+    bodyScrollLock.enableBodyScroll(document.body);
+  });
+
+})();
